87dde0f930c6316f71aa483f21dcded9db655a79
haot171620 <39385132+haot171620@users.noreply.github.com>
2019-05-29 22:30:43 +0700

    hao`
    hao`

--- PhuTungXeMay2019/Controllers/HomeController.cs
+++ PhuTungXeMay2019/Controllers/HomeController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PhuTungXeMay2019.Models;
 namespace PhuTungXeMay2019.Controllers
 {
     public class HomeController : Controller
     {
+        private CsK23T2bEntities db = new CsK23T2bEntities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.SanPhams.ToList());
         }
 
         public ActionResult About()
---
+++ PhuTungXeMay2019/Models/Cart1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhuTungXeMay2019.Models
+{
+    public class Cart1
+    {
+        CsK23T2bEntities db = new CsK23T2bEntities();
+        private int idSp1;
+
+        public Cart1(int idSp1)
+        {
+            // TODO: Complete member initialization
+            this.idSp1 = idSp1;
+        }
+        public int idSp { get; set; }
+        public int tenSp { get; set; }
+        public int dongia { get; set; }
+        public int soluong { get; set; }
+        public int tonkho { get; set; }
+        public int Quantity { get; set; }
+        public double thanhTien
+        {
+            get { return soluong * dongia; }
+
+        }
+    }
+}
